9ba024d784cc2209d65ebc4218e1fbc536adf41f
Andrew <adoyle1215@gmail.com>
2020-01-21 05:33:10 +0000

    doylead: Added some framework for video discovery script

--- database/python/timeseries/hourly/discover_videos.py
+++ database/python/timeseries/hourly/discover_videos.py
@@ -10,15 +10,37 @@ connstring = "dbname='kpds'"
 conn = psycopg2.connect(connstring)
 cur = conn.cursor()
 
+### This script to be called by CRON - Check scheduling!
+
 ### Pseudocode in-line for easier development
 
 ## Query DB for channels actively being tracked at this frequency
+channel_query = ('SELECT youtube_channel_id,youtube_channel_external_key '
+                    'FROM "YouTube_Channels" '
+                    'WHERE tracking=\'t\' AND scan_frequency=720 '
+                    'ORDER BY youtube_channel_id;')
+cur.execute(channel_query)
+channel_results = cur.fetchall()
+# Currently prints only SMTOWN, as expected
 
 ## Declare necessary logic variables
 
 ## Outer FOR loop through channels
+for channel_result in channel_results:
+    channel_id, external_key = channel_result
+    channel_id = (channel_id,) # Format for psycopg2
 
     ## Query DB for videos on this channel
+    video_query = ('SELECT youtube_video_id,youtube_video_external_key '
+                    'FROM "YouTube_Videos" '
+                    'WHERE youtube_channel_id=%s')
+    cur.execute(video_query,channel_id)
+    video_results = cur.fetchall()
+
+    for r in video_results:
+        print(r)
+
+    more_videos_on_channel = True
 
     ## WHILE loop - are there new videos on this channel?
 
