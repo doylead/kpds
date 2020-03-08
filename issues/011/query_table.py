6222ff5d7826f4f55148423f2939a7439b1f4b81
Andrew <adoyle1215@gmail.com>
2020-03-08 21:04:27 +0000

    doylead: Resolved Issue 11

--- issues/011/query_table.py
+++ issues/011/query_table.py
@@ -7,8 +7,18 @@ connstring = "dbname='kpds'"
 conn = psycopg2.connect(connstring)
 cur = conn.cursor()
 
+## Checks to see if a table with caps-sensitive schema and
+## table name exists
+# Works as intended
+query = sql.SQL("select * from pg_tables where schemaname=%s and tablename=%s")
+cur.execute(query,["YouTube_Video_Stats","Test"])
+results = cur.fetchall()
+exists = (len(results)==1)
+if not exists:
+    print("Table does not exist")
+
 ## Creates table with caps-sensitive schema and table name
-# WIP
+# Works as intended
 query = sql.SQL("create table {schema_table} (a INT, b INT)").format(
         schema_table = sql.Identifier("YouTube_Video_Stats","Test")
         )
@@ -17,11 +27,17 @@ conn.commit()
 
 ## Checks to see if a table with caps-sensitive schema and
 ## table name exists
-# WIP
+# Works as intended
+query = sql.SQL("select * from pg_tables where schemaname=%s and tablename=%s")
+cur.execute(query,["YouTube_Video_Stats","Test"])
+results = cur.fetchall()
+exists = (len(results)==1)
+if exists:
+    print("Table Exists")
 
 ## Adds a value to a table with a caps-sensitive schema
 ## and table name
-# Works for one column
+# Works as intended
 query = sql.SQL("insert into {schema_table} ({fields}) values (%s,%s)").format(
         schema_table = sql.Identifier("YouTube_Video_Stats","Test"),
         fields = sql.SQL(",").join([
